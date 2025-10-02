005a90e97f64571a0be15d55dd845bdf32c32032
Guysnacho <tunjiprod@gmail.com>
2025-10-01 20:33:33 -0500

    update presented asset

--- src/pages/listing/view.tsx
+++ src/pages/listing/view.tsx
@@ -1,16 +1,19 @@
+import { Box } from "@chakra-ui/react";
 import ReactPlayer from "react-player";
 
 export default function View() {
   return (
-    <ReactPlayer
-      muted={true}
-      autoPlay={true}
-      onError={() => alert("Failed to render property")}
-      className="mx-auto"
-      width="100vw"
-      height="100vh"
-      loop={false}
-      src="/assets/2025-10-01-19-00-33.mp4"
-    />
+    <Box h="100vh" className="overflow-clip">
+      <ReactPlayer
+        muted={true}
+        autoPlay={true}
+        onError={() => alert("Failed to render property")}
+        className="mx-auto"
+        width="100%"
+        height="auto"
+        loop
+        src="/assets/2025-10-01-19-00-33.mp4"
+      />
+    </Box>
   );
 }
